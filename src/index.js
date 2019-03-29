959cbc67480ada5699f166081cbffe48a8b6b476
Dom Dopico <domdopico@Doms-MacBook-Pro.local>
2019-03-29 15:57:54 -0700

    update Board status based on calculateWinner

--- src/index.js
+++ src/index.js
@@ -38,7 +38,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
